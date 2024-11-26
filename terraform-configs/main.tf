7b622deeaa28e8970f8875a6429edf91126655e5
profgreatwonder <muoghaluchidinma@gmail.com>
2024-11-26 09:15:53 +0100

    updated the run part of the Set Short SHA step in the ci.yaml file

--- terraform-configs/main.tf
+++ terraform-configs/main.tf
@@ -1,46 +1,46 @@
-# # minikube provider(a bridge that allows terraform able to control specific services, in this case, minikube) and cluster resource 
-# terraform {
-#   required_providers {
-#     minikube = {
-#         source = "scott-the-programmer/minikube"
-#         version = "0.4.2"
-#     }
-#   }
-# }
-
-# # required provider
-# provider "minikube" {
-#   kubernetes_version = "v1.30.0"
-# }
-
-# # minikube cluster resource
-# resource "minikube_cluster" "minikube_docker" {
-#     driver = "docker"
-#     cluster_name = "complete_devops_project"
-#     addons = [
-#         "default-storageclass",
-#         "storage-provisioner"
-#     ]
-# }
-
-terraform{
-    required_providers {
-      minikube = {
+# minikube provider(a bridge that allows terraform able to control specific services, in this case, minikube) and cluster resource 
+terraform {
+  required_providers {
+    minikube = {
         source = "scott-the-programmer/minikube"
         version = "0.4.2"
-      }
     }
+  }
 }
 
+# required provider
 provider "minikube" {
-    kubernetes_version = "v1.30.0"
+  kubernetes_version = "v1.30.0"
 }
 
+# minikube cluster resource
 resource "minikube_cluster" "minikube_docker" {
     driver = "docker"
-    cluster_name = "complete-devops-project"
+    cluster_name = "complete_devops_project"
     addons = [
         "default-storageclass",
         "storage-provisioner"
     ]
 }
+
+# terraform{
+#     required_providers {
+#       minikube = {
+#         source = "scott-the-programmer/minikube"
+#         version = "0.4.2"
+#       }
+#     }
+# }
+
+# provider "minikube" {
+#     kubernetes_version = "v1.30.0"
+# }
+
+# resource "minikube_cluster" "minikube_docker" {
+#     driver = "docker"
+#     cluster_name = "complete-devops-project"
+#     addons = [
+#         "default-storageclass",
+#         "storage-provisioner"
+#     ]
+# }
